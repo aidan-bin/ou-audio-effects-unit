1c325df07e96f9c61fdc298909eac703be46f0a6
aidan-bin <aidan-bin@users.noreply.github.com>
2026-05-25 23:58:08 -0700

    Remove redundant build in check workflow

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./scripts/build.sh
 ./scripts/test.sh
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j
 ctest --test-dir build --output-on-failure
