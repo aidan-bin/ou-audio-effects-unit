4d7cc7047322319769d9053421f2c32219b23369
aidan-bin <aidan-bin@users.noreply.github.com>
2026-05-31 21:39:42 -0700

    Remove compatibility header shims

--- firmware/stm32f303/app/include/effects_control_logic.h
+++
@@ -1,6 +0,0 @@
-#ifndef EFFECTS_CONTROL_LOGIC_H
-#define EFFECTS_CONTROL_LOGIC_H
-
-#include "effects_model.h"
-
-#endif
--- firmware/stm32f303/app/include/effects_state_manager.h
+++
@@ -1,6 +0,0 @@
-#ifndef EFFECTS_STATE_MANAGER_H
-#define EFFECTS_STATE_MANAGER_H
-
-#include "effects_model.h"
-
-#endif
